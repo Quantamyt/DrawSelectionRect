f37bc6249093f72b14deada00c57cb959338bf19
Quantamyt <Sujataghosal05@gmail.com>
2024-03-17 22:33:51 +0530

    Update Code

--- src/DrawingRectangles.java
+++ src/DrawingRectangles.java
@@ -8,5 +8,19 @@ public class DrawingRectangles extends JFrame {
     private int startX, startY, endX, endY;
     private boolean drawing;
 
+    public DrawingRectangles() {
+        setTitle("Draw Rectangles");
+        setSize(600, 400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
 
+
+
+
+
+
+
+
+
+    }
 }
